1cdc798b1d606cff9919474a71e51b492984c2c2
Allani <allanimohamedmahdi@gmail.com>
2016-01-07 03:09:14 +0000

    nouveau fichier: App.jsx 	nouveau fichier: index.html 	nouveau fichier: main.js

---
+++ App.jsx
@@ -0,0 +1,14 @@
+App.jsx
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            Hello World!!!
+         </div>
+      );
+   }
+}
+
+export default App;
